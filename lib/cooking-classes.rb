1cbf8740232dce12c37c5776bf1466e4aa6e65cc
Villy Siu <villysiu@gmail.com>
2017-03-21 13:43:45 -0700

    scrape the page for favorites

--- lib/cooking-classes.rb
+++ lib/cooking-classes.rb
@@ -7,5 +7,20 @@ class FavoriteCookingClasses::CookingClass
     @menu = menu
     @@all << self
   end
+  
+  def self.scrape_customer_favorites
 
+  #  html = open("http://www.surlatable.com/category/cat2211278/Find+a+Cooking+Class")
+    base_url = "http://www.surlatable.com/"
+    doc = Nokogiri::HTML(open(base_url + "category/cat2211278/Find+a+Cooking+Class"))
+
+    customer_favorites = doc.css("div.boxsides").first
+
+    customer_favorites.css("div.product a").each_with_index do |the_class, i|
+      favorite_class = self.new
+      favorite_class.name = the_class.children[1]['alt'].sub!('Cooking Class: ', '')
+      favorite_class.url = the_class['href']
+    end
+    @@all
+  end
 end
