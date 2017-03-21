35aff577a479fa0094e87b7dd7b9d221296248ac
Villy Siu <villysiu@gmail.com>
2017-03-21 15:08:40 -0700

    Create cooking_classes.rb

---
+++ lib/cooking_classes.rb
@@ -0,0 +1,7 @@
+require 'open-uri'
+require 'pry'
+require 'nokogiri'
+
+require_relative './cooking_classes/version'
+require_relative './cooking_classes/cli'
+require_relative './cooking_classes/cooking-classes'
