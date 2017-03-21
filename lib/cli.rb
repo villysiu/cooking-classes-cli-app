dda3efb880a1b6bf00d914fd2b5e220249ed28fa
Villy Siu <villysiu@gmail.com>
2017-03-21 14:39:40 -0700

    define menu

--- lib/cli.rb
+++ lib/cli.rb
@@ -11,7 +11,29 @@ class FavoriteCookingClasses::CLI
     FavoriteCookingClasses::CookingClass.all.each_with_index { |the_class, i| puts "#{i+1}. #{the_class.name}" }
   end
   def menu
-  
+    input = nil
+    while input != "exit"
+
+      puts "Enter the class number you'd like more details? Or enter 'list' to see the classes again or 'exit' to exit"
+
+      input = gets.strip.downcase
+      if input == "list"
+        list_favorite_classes
+      elsif input.to_i > 0 && input.to_i <= FavoriteCookingClasses::CookingClass.all.size
+        favorite_class = FavoriteCookingClasses::CookingClass.find_class(input.to_i)
+        puts "********* #{input}. #{favorite_class.name} *********"
+        puts "Description:"
+        puts favorite_class.description
+        puts " "
+        puts "Menu:"
+        puts favorite_class.menu
+        puts " "
+      elsif input == "exit"
+        goodbye
+      else
+        puts "This is not a valid class. Try again."
+      end
+    end
   end
   def goodbye
     puts "Goodbye"
